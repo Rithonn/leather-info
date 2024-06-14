df6f82e087da0ae98a362dc878aa8e81f50a1b5c
Collin <cbrandt6@ycp.edu>
2024-06-14 08:47:01 -0400

    Adds file to query youtube API

---
+++ src/routes/creators/+page.server.ts
@@ -0,0 +1,7 @@
+import { GOOGLE_API_KEY } from '$env/static/private';
+import { YoutubeCreator } from '$lib/interfaces/ContentCreator';
+
+export const load = () => {
+    const creators = ['@80ProofGoods'];
+    return {}
+}
